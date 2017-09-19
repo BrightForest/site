49129d8569eb7d66917b1e1a334e61f2af850067
Bylevskiy <bylevskiy@restream.rt.ru>
2017-09-19 16:07:09 +0300

    Change main

--- src/site/main.go
+++ src/site/main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-"fmt"
-"net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test %s", r.URL.Path[1:])
+func mainPageView(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
+	// attention: If you do not call ParseForm method, the following data can not be obtained form
+	fmt.Println(r.Form) // print information on server side.
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "This is main page of site.") // write data to response
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("./src/site/login.gptl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		getUserLogin := r.Form["username"]
+		getUserPassword := r.Form["password"]
+		if (getUserLogin[0] == "admin") && (getUserPassword[0] == "admin") {
+			fmt.Fprintln(w, "Fucking right.")
+		} else{
+			fmt.Fprintln(w, "Wrong password.")
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", mainPageView) // setting router rule
+	http.HandleFunc("/admin", login)
+	err := http.ListenAndServe(":8080", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
